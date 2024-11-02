933d396739d43b73e4699c1b502c4dccea08e1d1
Ameya Mhatre <ameyam.projects@gmail.com>
2024-11-02 19:08:33 +0530

    added table content

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         <h3  className='notification'>Notifications</h3>
       </div>
       <a href="https://slcm.manipal.edu/UplodedFilePath/EMI_Monthly_Fee_MAHE.pdf" className="hyperlink-1">👉 Introduction of EMI (Monthly) Fee Payment Option for Senior Students at MAHE</a>
-      <a href="link_to_hostel_policy"  className="hyperlink-1">👉 Hostel allotment policy for seniors 2024-25 - Revised</a>
+      <a href="https://slcm.manipal.edu/UplodedFilePath/Hostel_allotment_policy_for_seniors_2024-25.pdf"  className="hyperlink-1">👉 Hostel allotment policy for seniors 2024-25 - Revised</a>
       {/* <div className="search-bar">
         <input
           type="text"
@@ -29,10 +29,52 @@ function App() {
       <table className="table">
         <thead>
           <tr>
-            <th>Event</th>
-            <th>Subject</th>
-            <th>Type</th>
-            <th>Details</th>
+            <td>Event</td>
+            <td>Subject</td>
+            <td>Type</td>
+            <td>Details</td>
+          </tr>
+          <tr>
+            <th>BROADCAST</th>
+            <th>MIT BLR 23-24 BATCH REVISED FEE NOTIFICATION & SCHEDULE FOR THE ACADEMIC YEAR 2024-25</th>
+            <th>Important Document</th>
+            <th></th>
+          </tr>
+          <tr>
+            <th>BROADCAST</th>
+            <th>BANK DETAILS FOR USD PAYMENT</th>
+            <th>Important Document</th>
+            <th></th>
+          </tr>
+          <tr>
+            <th>BROADCAST</th>
+            <th>MIT BLR BTEC 23-24 BATCH ECE FEE NOTIFICATION & SCHEDULE FOR THE ACADEMIC YEAR 2024-25</th>
+            <th>Important Document</th>
+            <th></th>
+          </tr>
+          <tr>
+            <th>BROADCAST</th>
+            <th>MMIT BLR NRI 2 FEE NOTIFICATION & SCHEDULE FOR THE ACADEMIC YEAR 2024-25 </th>
+            <th>Important Document</th>
+            <th></th>
+          </tr>
+          <tr>
+            <th>BROADCAST</th>
+            <th> 	MIT BLR GEN 3 FEE NOTIFICATION & SCHEDULE FOR THE ACADEMIC YEAR 2024-25 </th>
+            <th>Important Document</th>
+            <th></th>
+          </tr>
+          <tr>
+            <th>BROADCAST</th>
+            <th>MIT BLR NRI 1 FEE NOTIFICATION & SCHEDULE FOR THE ACADEMIC YEAR 2024-25 </th>
+            <th>Important Document</th>
+            <th></th>
+          </tr>
+          <tr>
+            <th>BROADCAST</th>
+            <th>MIT BLR GEN 2 FEE NOTIFICATION & SCHEDULE FOR THE ACADEMIC YEAR 2024-25 </th>
+            <th>Important Document</th>
+            <th></th>
           </tr>
         </thead>
         <tbody id="tableBody">
